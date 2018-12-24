16b0f8249b7540725a89b5b11dbae0e936c4e120
Yoshiyuki Ieyama <yukku0423@gmail.com>
2018-12-24 19:19:44 +0900

    improve prompt for "mosaic info"

--- src/commands/mosaic/info.ts
+++ src/commands/mosaic/info.ts
@@ -16,10 +16,12 @@
  *
  */
 import chalk from 'chalk';
-import {command, ExpectedError, metadata, option,} from 'clime';
-import {AccountHttp, MosaicHttp, MosaicId, MosaicService, NamespaceHttp,} from 'nem2-sdk';
-import {ProfileCommand, ProfileOptions} from '../../profile.command';
+import {command, ExpectedError, metadata, option} from 'clime';
+import {AccountHttp, MosaicHttp, MosaicId, MosaicService, NamespaceHttp} from 'nem2-sdk';
+import prompt from '../../inquirerHelper';
+import * as validator from '../../inquirerHelper/validator';
 import {OptionsResolver} from '../../options-resolver';
+import {ProfileCommand, ProfileOptions} from '../../profile.command';
 
 export class CommandOptions extends ProfileOptions {
     @option({
@@ -45,21 +47,31 @@ export default class extends ProfileCommand {
     }
 
     @metadata
-    execute(options: CommandOptions) {
-        this.spinner.start();
+    async execute(options: CommandOptions) {
         const profile = this.getProfile(options);
 
         if (!options.uint) {
-            options.name = OptionsResolver(options,
+            options.name = await OptionsResolver(options,
                 'name',
                 () => undefined,
-                'Introduce the mosaic name: ');
+                'Introduce the mosaic name:',
+                prompt({
+                    type: 'input',
+                    validate: (input) => input.length === 0 || validator.mosaicFqn()(input),
+                }),
+            );
         }
         if (options.name === '') {
-            options.uint = OptionsResolver(options,
+            options.uint = await OptionsResolver(options,
                 'uint',
                 () => undefined,
-                'Introduce the mosaic id in uint64 format. [number, number]: ');
+                'Introduce the mosaic id in uint64 format. [number, number]:',
+                prompt({
+                    type: 'input',
+                    filter: (input: string) => input.trim().replace(/\s+/g, ' '),
+                    validate: validator.uint64(),
+                }),
+            );
         }
 
         let mosaicId: MosaicId;
@@ -77,6 +89,7 @@ export default class extends ProfileCommand {
             new NamespaceHttp(profile.url),
         );
 
+        this.spinner.start();
         mosaicService.mosaicsView([mosaicId])
             .subscribe((mosaicViews) => {
                 this.spinner.stop(true);
