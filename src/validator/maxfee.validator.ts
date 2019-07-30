95676815af099fb75a9f0b2be4aee605c5452151
dnice <dnice_bo@Outlook.com>
2019-07-30 09:35:08 +0800

    Nonnegative number

--- src/validator/maxfee.validator.ts
+++ src/validator/maxfee.validator.ts
@@ -2,7 +2,7 @@ import {ExpectedError, ValidationContext, Validator} from 'clime';
 
 export class MaxfeeValidator implements Validator<string> {
     validate(value: string, context: ValidationContext): void {
-        const  tag = /^(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*))$/;
+        const  tag = /^\d+$/;
         if (!tag.test(value)) {
             throw new ExpectedError('maxFee must be greater than or equal to 0');
         }
