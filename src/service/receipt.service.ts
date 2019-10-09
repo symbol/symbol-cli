fd6630793008f8f6daca974480efbacb3d74ce83
nemtechCN <527357637@qq.com>
2019-10-09 17:05:01 +0800

    render receipts using tables

--- src/service/receipt.service.ts
+++ src/service/receipt.service.ts
@@ -1,6 +1,9 @@
 import chalk from 'chalk';
+import {HorizontalTable} from 'cli-table3';
+import * as Table from 'cli-table3';
 import {
     Address,
+    AddressAlias,
     ArtifactExpiryReceipt,
     BalanceChangeReceipt,
     BalanceTransferReceipt,
@@ -12,8 +15,24 @@ import {
 } from 'nem2-sdk';
 
 export class ReceiptService {
+    private transactionStatementsTable: HorizontalTable;
+    private addressResolutionStatementsTable: HorizontalTable;
+    private mosaicResolutionStatementsTable: HorizontalTable;
     constructor() {
+        this.transactionStatementsTable = new Table({
+            style: {head: ['cyan']},
+            head: ['Version', 'Type', 'RecipientAddress', 'SenderPublickey', 'TargetPublicKey', 'MosaicId', 'Amount'],
+        }) as HorizontalTable;
 
+        this.addressResolutionStatementsTable = new Table({
+            style: {head: ['cyan']},
+            head: ['Resolved', 'PrimaryId', 'SecondaryId'],
+        }) as HorizontalTable;
+
+        this.mosaicResolutionStatementsTable = new Table({
+            style: {head: ['cyan']},
+            head: ['Resolved', 'PrimaryId', 'SecondaryId'],
+        }) as HorizontalTable;
     }
 
     /**
@@ -24,41 +43,53 @@ export class ReceiptService {
     public formatTransactionStatements(statement: any): string {
         let txt = '';
         txt += chalk.green('transactionStatements:\t') + '\n';
-        txt += '-'.repeat('transactionStatements:\t'.length) + '\n\n';
         statement.transactionStatements.map((transaction: TransactionStatement, transactionIndex: number) => {
-            txt += 'height:\t\t' + transaction.height + '\n';
             transaction.receipts.map((receipt: any, receiptIndex: number) => {
-                txt += '<index: ' + transactionIndex + '-' + receiptIndex + '>\t';
                 if (receipt instanceof BalanceTransferReceipt) {
-                    txt += 'version:\t' + receipt.version + '\n';
-                    txt += '\t\ttype:\t\t' + ReceiptType[receipt.type]  + '\n';
-                    txt += '\t\trecipientAddress:\t' +
+                    this.transactionStatementsTable.push([
+                        receipt.version as Table.CellOptions,
+                        ReceiptType[receipt.type],
                         (receipt.recipientAddress instanceof Address ?
-                            receipt.recipientAddress.pretty() : receipt.recipientAddress.toHex()) + '\n';
-                    txt += '\t\tsenderPublickey:\t' + receipt.sender.publicKey + '\n';
-                    txt += '\t\tmosaicId:\t[ ' + receipt.mosaicId.id.lower + ', ' + receipt.mosaicId.id.higher + ' ]\n\n';
-                    txt += '\t\tamount:\t\t' + receipt.amount.compact() + '\n';
+                            receipt.recipientAddress.pretty() : receipt.recipientAddress.toHex()),
+                        receipt.sender.publicKey,
+                        '',
+                        '[ ' + receipt.mosaicId.id.lower + ', ' + receipt.mosaicId.id.higher + ' ]',
+                        receipt.amount.compact(),
+                    ]);
                 } else if (receipt instanceof BalanceChangeReceipt) {
-                    txt += 'version:\t' + receipt.version + '\n';
-                    txt += '\t\ttype:\t\t' + ReceiptType[receipt.type] + '\n';
-                    txt += '\t\ttargetPublicKey:\t' + receipt.targetPublicAccount.publicKey + '\n';
-                    txt += '\t\tmosaicId:\t[ ' + receipt.mosaicId.id.lower + ', '
-                        + receipt.mosaicId.id.higher + ' ]\n\n';
-                    txt += '\t\tamount:\t\t' + receipt.amount.compact() + '\n';
+                    this.transactionStatementsTable.push([
+                        receipt.version as Table.CellOptions,
+                        ReceiptType[receipt.type],
+                        '',
+                        '',
+                        receipt.targetPublicAccount.publicKey,
+                        '[ ' + receipt.mosaicId.id.lower + ', ' + receipt.mosaicId.id.higher + ' ]',
+                        receipt.amount.compact(),
+                    ]);
                 }  else if (receipt instanceof ArtifactExpiryReceipt) {
-                    txt += 'version:\t' + receipt.version + '\n';
-                    txt += '\t\ttype:\t\t' + ReceiptType[receipt.type]  + '\n';
-                    txt += '\t\tmosaicId:\t[ ' + receipt.artifactId.id.lower + ', '
-                        + receipt.artifactId.id.higher + ' ]\n\n';
+                    this.transactionStatementsTable.push([
+                        receipt.version as Table.CellOptions,
+                        ReceiptType[receipt.type],
+                        '',
+                        '',
+                        '',
+                        '[ ' + receipt.artifactId.id.lower + ', ' + receipt.artifactId.id.higher + ' ]',
+                        '',
+                    ]);
                 }  else if (receipt instanceof InflationReceipt) {
-                    txt += 'version:\t' + receipt.version + '\n';
-                    txt += '\t\ttype:\t\t' + ReceiptType[receipt.type]  + '\n';
-                    txt += '\t\tamount:\t\t' + receipt.amount.compact() + '\n';
-                    txt += '\t\tmosaicId:\t[ ' + receipt.mosaicId.id.lower + ', '
-                        + receipt.mosaicId.id.higher + ' ]\n\n';
+                    this.transactionStatementsTable.push([
+                        receipt.version as Table.CellOptions,
+                        ReceiptType[receipt.type],
+                        '',
+                        '',
+                        '',
+                        '[ ' + receipt.mosaicId.id.lower + ', ' + receipt.mosaicId.id.higher + ' ]',
+                        receipt.amount.compact(),
+                    ]);
                 }
             });
         });
+        txt += this.transactionStatementsTable.toString() + '\n\n';
         return txt;
     }
 
@@ -70,17 +101,17 @@ export class ReceiptService {
     public formatAddressResolutionStatements(statement: any): string {
         let txt = '';
         txt += chalk.green('addressResolutionStatements:\t') + '\n';
-        txt += '-'.repeat('addressResolutionStatements:\t'.length) + '\n\n';
         statement.addressResolutionStatements.map((addressResolution: ResolutionStatement, addressResolutionIndex: number) => {
-            txt += 'height:\t\t' + addressResolution.height + '\n';
-            txt += 'unresolved:\t' + addressResolution.unresolved + '\n\n';
             addressResolution.resolutionEntries.map((resolutionEntry: ResolutionEntry, resolutionEntryIndex: number) => {
-                txt += '<index:' + addressResolutionIndex + '-' + resolutionEntryIndex + '>\t';
-                txt += 'resolved:\t\t' + resolutionEntry.resolved + '\n';
-                txt += '\t\tprimaryId:\t\t' + resolutionEntry.source.primaryId + '\n';
-                txt += '\t\tsecondaryId:\t\t' + resolutionEntry.source.secondaryId + '\n\n';
+                this.addressResolutionStatementsTable.push([
+                    resolutionEntry.resolved instanceof AddressAlias ?
+                        resolutionEntry.resolved.address.pretty() : resolutionEntry.resolved.mosaicId.toHex(),
+                    resolutionEntry.source.primaryId,
+                    resolutionEntry.source.secondaryId,
+                ]);
             });
         });
+        txt += this.addressResolutionStatementsTable.toString() + '\n\n';
         return txt;
     }
 
@@ -92,17 +123,17 @@ export class ReceiptService {
     public formatMosaicResolutionStatements(statement: any): string {
         let txt = '';
         txt += chalk.green('mosaicResolutionStatements:\t') + '\n';
-        txt += '-'.repeat('mosaicResolutionStatements:\t'.length) + '\n\n';
         statement.mosaicResolutionStatements.map((mosaicResolution: ResolutionStatement, mosaicResolutionIndex: number) => {
-            txt += 'height:\t\t' + mosaicResolution.height + '\n';
-            txt += 'unresolved:\t' + mosaicResolution.unresolved + '\n\n';
             mosaicResolution.resolutionEntries.map((resolutionEntry: ResolutionEntry, resolutionEntryIndex: number) => {
-                txt += '<index:' + mosaicResolutionIndex + '-' + resolutionEntryIndex + '>\t';
-                txt += 'resolved:\t\t' + resolutionEntry.resolved + '\n';
-                txt += '\t\tprimaryId:\t\t' + resolutionEntry.source.primaryId + '\n';
-                txt += '\t\tsecondaryId:\t\t' + resolutionEntry.source.secondaryId + '\n\n';
+                this.mosaicResolutionStatementsTable.push([
+                    resolutionEntry.resolved instanceof AddressAlias ?
+                        resolutionEntry.resolved.address.pretty() : resolutionEntry.resolved.mosaicId.toHex(),
+                    resolutionEntry.source.primaryId,
+                    resolutionEntry.source.secondaryId,
+                ]);
             });
         });
+        txt += this.mosaicResolutionStatementsTable.toString() + '\n\n';
         return txt;
     }
 }
