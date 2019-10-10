9d9a36c1f0c4f210a8ec155595e7306d5e4effc4
nemtechCN <527357637@qq.com>
2019-10-10 14:28:19 +0800

    support transfer transaction encrypted messages

--- src/commands/transaction/transfer.ts
+++ src/commands/transaction/transfer.ts
@@ -15,13 +15,15 @@
  * limitations under the License.
  *
  */
-import {command, ExpectedError, metadata, option} from 'clime';
-import {Address, Deadline, Mosaic, NamespaceId, PlainMessage, TransferTransaction, UInt64} from 'nem2-sdk';
-import {AnnounceTransactionsCommand, AnnounceTransactionsOptions} from '../../announce.transactions.command';
-import {OptionsResolver} from '../../options-resolver';
-import {MosaicService} from '../../service/mosaic.service';
-import {AddressValidator} from '../../validators/address.validator';
-import {MosaicsValidator} from '../../validators/mosaic.validator';
+import { command, ExpectedError, metadata, option } from 'clime';
+import { Address, Deadline, Mosaic, NamespaceId, PlainMessage, PublicAccount, TransferTransaction, UInt64 } from 'nem2-sdk';
+import { AnnounceTransactionsCommand, AnnounceTransactionsOptions } from '../../announce.transactions.command';
+import { OptionsResolver } from '../../options-resolver';
+import { MosaicService } from '../../service/mosaic.service';
+import { AddressValidator } from '../../validators/address.validator';
+import { BinaryValidator } from '../../validators/binary.validator';
+import { MosaicsValidator } from '../../validators/mosaic.validator';
+import { PublicKeyValidator } from '../../validators/publicKey.validator';
 
 export class CommandOptions extends AnnounceTransactionsOptions {
     @option({
@@ -43,6 +45,22 @@ export class CommandOptions extends AnnounceTransactionsOptions {
         validator: new MosaicsValidator(),
     })
     mosaics: string;
+
+    @option({
+        flag: 'e',
+        description: '(Optional) Send an encrypted message ' +
+            '[1: encrypted message, 0: normal message] ' +
+            '(If you set this value, you should set the value of \'recipientPublicKey\' as well).',
+        validator: new BinaryValidator(),
+    })
+    encrypted: number;
+
+    @option({
+        flag: 'p',
+        description: '(Optional) The recipient public key in an encrypted message.',
+        validator: new PublicKeyValidator(),
+    })
+    recipientPublicKey: string;
 
 }
 
@@ -74,6 +92,7 @@ export default class extends AnnounceTransactionsCommand {
             () => undefined,
             'Mosaics to transfer in the format (mosaicId(hex)|@aliasName)::absoluteAmount,' +
             ' (Ex: sending 1 cat.currency, @cat.currency::1000000). Add multiple mosaics with commas: > ');
+
         if (options.mosaics) {
             mosaics = MosaicService.getMosaics(options.mosaics);
         }
@@ -89,8 +108,35 @@ export default class extends AnnounceTransactionsCommand {
             () => undefined,
             'Introduce the maximum fee you want to spend to announce the transaction: ');
 
-        const transferTransaction = TransferTransaction.create(Deadline.create(), recipient, mosaics,
-            PlainMessage.create(message), profile.networkType, UInt64.fromUint(options.maxFee));
+        let transferTransaction;
+        if (1 === options.encrypted) {
+            options.recipientPublicKey = OptionsResolver(options,
+                'recipientPublicKey',
+                () => undefined,
+                'Introduce the recipient public key in an encrypted message: ');
+
+            const recipientAccount = PublicAccount.createFromPublicKey(options.recipientPublicKey, profile.networkType);
+            const encryptedMessage = profile.account
+                .encryptMessage(options.message,
+                    recipientAccount,
+                    profile.networkType);
+            transferTransaction = TransferTransaction.create(
+                Deadline.create(),
+                recipient,
+                mosaics,
+                encryptedMessage,
+                profile.networkType,
+                UInt64.fromUint(options.maxFee),
+            );
+        } else {
+            transferTransaction = TransferTransaction.create(
+                Deadline.create(),
+                recipient,
+                mosaics,
+                PlainMessage.create(message),
+                profile.networkType,
+                UInt64.fromUint(options.maxFee));
+        }
 
         const signedTransaction = profile.account.sign(transferTransaction, profile.networkGenerationHash);
         this.announceTransaction(signedTransaction, profile.url);
