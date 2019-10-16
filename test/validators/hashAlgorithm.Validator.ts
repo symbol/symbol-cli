448dc9fc61e239661a2b6a72053bdea8c7c8d016
nemtechCN <527357637@qq.com>
2019-10-16 16:56:13 +0800

    fix test describe

--- test/validators/hashAlgorithm.Validator.ts
+++ test/validators/hashAlgorithm.Validator.ts
@@ -1,29 +1,28 @@
 import {expect} from 'chai';
-import {HashType} from 'nem2-sdk';
 import {HashAlgorithmValidator} from '../../src/validators/hashAlgorithm.Validator';
 
 describe('hashAlgorithm value', () => {
-    it('Invalid value: 0', () => {
+    it('Valid value: 0', () => {
         const value = 0;
         expect(new HashAlgorithmValidator().validate(value, {name: 'value', source: value.toString()})).to.be.equal(undefined);
     });
 
-    it('Invalid value: 1', () => {
+    it('Valid value: 1', () => {
         const value = 1;
         expect(new HashAlgorithmValidator().validate(value, {name: 'value', source: String(value)})).to.be.equal(undefined);
     });
 
-    it('Invalid value: 2', () => {
+    it('Valid value: 2', () => {
         const value = 2;
         expect(new HashAlgorithmValidator().validate(value, {name: 'value', source: String(value)})).to.be.equal(undefined);
     });
 
-    it('Invalid value: 3', () => {
+    it('Valid value: 3', () => {
         const value = 3;
         expect(new HashAlgorithmValidator().validate(value, {name: 'value', source: String(value)})).to.be.equal(undefined);
     });
 
-    it('Uninvalid value: 10', () => {
+    it('Invalid value: 10', () => {
         const value = 10;
         expect(() => {
             new HashAlgorithmValidator().validate(value, {name: 'value', source: String(value)});
