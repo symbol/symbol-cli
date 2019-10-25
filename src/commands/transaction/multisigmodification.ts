9f7153bbbdd0f0fe99b570d0d7518ec8166b819e
NemtechCN <527357637@qq.com>
2019-10-25 17:38:09 +0800

    resolve compile error

--- src/commands/transaction/multisigmodification.ts
+++ src/commands/transaction/multisigmodification.ts
@@ -107,7 +107,7 @@ export default class extends AnnounceAggregateTransactionsCommand {
             () => undefined,
             'Introduce the maximum fee you want to spend to announce the hashlock transaction: ');
 
-        const profile = this.getProfile(options);
+        const profile = this.getProfile();
 
         const multisigAccount = PublicAccount.createFromPublicKey(options.multisigAccountPublicKey, profile.networkType);
         const newCosignatoryAccount = PublicAccount.createFromPublicKey(options.cosignatoryPublicKey, profile.networkType);
