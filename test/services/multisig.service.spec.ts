882b97b8705a5949e8b53eadbf9df36adb57ffc8
decentraliser <decentraliser@gmail.com>
2020-01-22 14:48:14 +0800

    save WIP

--- test/services/multisig.service.spec.ts
+++ test/services/multisig.service.spec.ts
@@ -16,10 +16,37 @@
  *
  */
 import {expect} from 'chai';
-import {SequentialFetcher} from '../../src/services/multisig.service.ts';
+import {Address} from 'nem2-sdk';
+import {instance, mock, times, verify, when} from 'ts-mockito';
+import {SequentialFetcher} from '../../src/services/multisig.service';
+
+const addresses = [
+    Address.createFromRawAddress('SAY7N2GP6JJBFIRBTUEXY2JJVOLGIZ46KWIMYB5T'),
+    Address.createFromRawAddress('SB2FRRM3SYAMQL47RRUKMQSRJXJT3QPVAVWNTXQX'),
+    Address.createFromRawAddress('SBIWHDWZMPIXXM2BINCRXAK3H3MGA5VHB3D2PO5W'),
+];
+
+const networkCall = (address: Address) => Promise.resolve(address);
+const mockNetworkCall = mock(networkCall);
 
 describe('Multisig service', () => {
-    it('should be true', () => {
-        expect(true).to.be.equal(true);
+    it('should call the response handler with all the addresses', async (done) => {
+        const sequentialFetcher = SequentialFetcher.create(mockNetworkCall, 1);
+        sequentialFetcher
+            .getTransactionsToCosign(addresses)
+            .subscribe(
+                (x) => console.log(x),
+                (err) => console.log(err),
+            );
+
+        // mockNetworkCall.
+        // setTimeout(() => {
+        // expect(mockNetworkCall.mock.calls[0][0]).equal([addresses[0]]);
+        // expect(mockNetworkCall.mock.calls[1][0]).equal([addresses[1]]);
+        // expect(mockNetworkCall.mock.calls[2][0]).equal([addresses[2]]);
+        // done();
+        // }, 50);
+
+        done();
     });
 });
