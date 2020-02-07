c684d3b7572665e3b798bd3e6ed51ee872cebccf
dgarcia360 <dgarcia360@outlook.com>
2020-02-07 09:45:32 +0000

    Fix order

--- src/commands/account/aggregatebonded.ts
+++ src/commands/account/aggregatebonded.ts
@@ -35,8 +35,8 @@ export default class extends AccountTransactionsCommand {
     async execute(options: AccountTransactionsOptions) {
         this.spinner.start()
         const profile = this.getProfile(options)
-        const accountHttp =  new AccountHttp(profile.url)
         const address = await new AddressResolver().resolve(options, profile)
+        const accountHttp =  new AccountHttp(profile.url)
 
         accountHttp.getAccountPartialTransactions(address, options.getQueryParams())
             .subscribe((transactions) => {
