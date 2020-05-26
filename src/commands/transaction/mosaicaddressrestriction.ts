aabf041b3e76e8b4a13faec4acb8ef037a13e628
dgarcia360 <dgarcia360@outlook.com>
2020-05-26 17:25:38 +0100

    Fixed pending commands

--- src/commands/transaction/mosaicaddressrestriction.ts
+++ src/commands/transaction/mosaicaddressrestriction.ts
@@ -16,7 +16,7 @@
  *
  */
 import { command, metadata, option } from 'clime';
-import { Deadline, MosaicRestrictionTransactionService, NamespaceHttp, RestrictionMosaicHttp } from 'symbol-sdk';
+import { Deadline, MosaicRestrictionTransactionService } from 'symbol-sdk';
 
 import { AnnounceTransactionsCommand } from '../../interfaces/announce.transactions.command';
 import { AnnounceTransactionsOptions } from '../../interfaces/announceTransactions.options';
@@ -79,8 +79,9 @@ export default class extends AnnounceTransactionsCommand {
         const maxFee = await new MaxFeeResolver().resolve(options);
         const signerMultisigInfo = await this.getSignerMultisigInfo(options);
 
-        const restrictionMosaicHttp = new RestrictionMosaicHttp(profile.url);
-        const namespaceHttp = new NamespaceHttp(profile.url);
+        const repositoryFactory = profile.repositoryFactory;
+        const restrictionMosaicHttp = repositoryFactory.createRestrictionMosaicRepository();
+        const namespaceHttp = repositoryFactory.createNamespaceRepository();
         const mosaicRestrictionTransactionService = new MosaicRestrictionTransactionService(restrictionMosaicHttp, namespaceHttp);
 
         const mosaicAddressRestrictionTransaction = await mosaicRestrictionTransactionService
